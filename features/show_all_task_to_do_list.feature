1f11301d8c50bd6cc2cfa377e3376f4e379d6be1
adelovelace <anmero@fiec.espol.edu.ec>
2023-08-01 16:51:04 -0500

    change feature 2 test

--- features/show_all_task_to_do_list.feature
+++ features/show_all_task_to_do_list.feature
@@ -9,5 +9,10 @@ Feature: List all tasks in the to-do list
         | Clean the bathroom        | 08-08-2023 | NOT_COMPLETED | HIGH     |
         When the user wants to see the to-do list
         Then show all the to-do list 
-        And print all the to-do list is
+        And print all the to-do list
+        | DESCRIPTION               | DATE       | STATUS        | PRIORITY |
+        | Buy Book: Animal Farm     | 23-07-2023 | COMPLETED     | LOW      |
+        | Buy groceries             | 01-08-2023 | NOT_COMPLETED | MEDIUM   |
+        | SE homework               | 31-07-2023 | IN_PROGRESS   | HIGH     |
+        | Clean the bathroom        | 08-08-2023 | NOT_COMPLETED | HIGH     |
         
--- features/steps/scn2_list_task.py
+++ features/steps/scn2_list_task.py
@@ -1,6 +1,7 @@
 from behave import *
 from src.todo.todo import TodoList
 from src.task.task import Task
+
 # Condiciones antes de empezar cualquier STEP
 def before_scenario(context, scenario):
     context = {}
@@ -13,7 +14,7 @@ def step_impl(context):
     to_do_list  = TodoList()
 
     for row in context.table:
-        to_do_list.add_task(row["DESCRIPTION"],row["PRIORITY"])
+        to_do_list.add_task(row["DESCRIPTION"],row["DATE"],row["PRIORITY"])
 
     context.to_do_list = to_do_list
 
@@ -35,5 +36,10 @@ def step_impl(context):
     # Print all the to-do list
     global to_do_list
     to_do_list = context.to_do_list
+    tasks_list = TodoList()
+
+    for row in context.table:
+        tasks_list.add_task(row["DESCRIPTION"],row["DATE"],row["PRIORITY"])
+
     assert to_do_list.list_tasks(), f'It does not print all the to-do list'
     
