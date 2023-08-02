4abab63febc2df65933591577ac0163277b1a738
practicatto <practicatto@gmail.com>
2023-08-01 23:40:22 -0500

    add error handling

--- src/task/task.py
+++ src/task/task.py
@@ -1,13 +1,42 @@
 from datetime import datetime
+from utils.custom_ex import InvalidPriority, InvalidStatus
 from utils.enums import Status, Priority
+from utils.validator import Validator
 
 class Task():
     def __init__(self, description, due_date ,priority="LOW"):
         self.description = description
         self.due_date = datetime.strptime(due_date, "%d-%m-%Y").date().strftime("%d-%m-%Y")
         self.status = Status.NOT_COMPLETED
-        self.priority = Priority[priority]
+        try:
+            self.priority = Priority[priority]
+        except KeyError:
+            raise InvalidPriority
 
+    @property
+    def description(self):
+        return self._description
+    
+    @description.setter
+    def description(self, description):
+        if not description:
+            raise ValueError("Description cannot be empty")
+        self._description = description
+    
+    @property
+    def due_date(self):
+        return self._due_date
+    
+    @due_date.setter
+    def due_date(self, due_date):
+        if not due_date:
+            raise ValueError("Due date cannot be empty")
+        try:
+            datetime.strptime(due_date, "%d-%m-%Y")
+        except ValueError:
+            raise ValueError("Invalid date format. Please use DD-MM-YYYY")
+        self._due_date = due_date
+    
     def __str__(self):
         return f"Task:\nDescription:{self.description} Priority: {self.priority.name} Status: {self.status.name} Due to: {self.due_date} "
 
--- src/todo/todo.py
+++ src/todo/todo.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 from utils.enums import Status
+from utils.validator import Validator
 
 class TodoList():
     def __init__(self):
         self.tasks = []
+        self.validator = Validator()
 
     def add_task(self, task):
         self.tasks.append(task)
@@ -13,24 +15,31 @@ class TodoList():
             print(task)
 
     def mark_task_completed(self, description):
+        self.validator.validate_description(description)
         for task in self.tasks:
             if task.description == description:
                 task.status = Status.COMPLETED
                 break
 
+
     def mark_task_in_progress(self, description):
+        self.validator.validate_description(description)
         for task in self.tasks:
             if task.description == description:
                 task.status = Status.IN_PROGRESS
                 break
 
     def mark_task_incomplete(self, description):
+        self.validator.validate_description(description)
         for task in self.tasks:
             if task.description == description:
                 task.status = Status.NOT_COMPLETED
                 break
 
     def get_tasks_by_date_range(self, start_date, end_date):
+        self.validator.validate_date(start_date)
+        self.validator.validate_date(end_date)
+
         by_date_range = []
         start_date = datetime.strptime(start_date, "%d-%m-%Y").date().strftime("%d-%m-%Y")
         end_date = datetime.strptime(end_date, "%d-%m-%Y").date().strftime("%d-%m-%Y")
@@ -41,6 +50,7 @@ class TodoList():
         return by_date_range
 
     def get_tasks_by_priority(self, priority):
+        self.validator.validate_priority(priority)
         by_priority = []
         for task in self.tasks:
             if task.priority.name == priority:
@@ -49,6 +59,7 @@ class TodoList():
         return by_priority
 
     def get_tasks_by_status(self, status):
+        self.validator.validate_status(status)
         by_status = []
         for task in self.tasks:
             if task.status.name == status:
@@ -57,12 +68,14 @@ class TodoList():
         return by_status
 
     def get_tasks_by_description(self, description):
+        self.validator.validate_description(description)
         for task in self.tasks:
             if task.description == description:
                 return task
         return None
 
     def delete_task(self, description):
+        self.validator.validate_description(description)
         for i,task in enumerate(self.tasks):
             if task.description == description:
                 del self.tasks[i]
